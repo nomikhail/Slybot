293cfe9176de741d0b5b9899460f941e02a13ec3
Misha2Kota <Misha2Kota@4425129e-acc9-b64f-aca5-168fcf8aa1ea>
2010-04-30 15:52:56 +0000

    log stakans for Rim and Micex index
    git-svn-id: https://laptop-i936umau/svn/Slybot@25 4425129e-acc9-b64f-aca5-168fcf8aa1ea

--- branches/slybot2/Service/Program.cs
+++ branches/slybot2/Service/Program.cs
@@ -10,126 +10,39 @@ namespace Service
 {
     class Program
     {
+        private static SQLiteConnection _connection = null;
+
         static void Main()
         {
-            var builder = new SQLiteConnectionStringBuilder {DataSource = @"D:\!\Database\quik_sqlite.db3"};
+            var builder = new SQLiteConnectionStringBuilder {DataSource = @"R:\quik_sqlite.db3"};
 
-            var connection = new SQLiteConnection(builder.ConnectionString);
-            connection.Open();
+            _connection = new SQLiteConnection(builder.ConnectionString);
+            _connection.Open();
+
+            Stakan._connection = _connection;
 
 
             var sw = new Stopwatch(); sw.Start();
             int total = 0;
             while (!Console.KeyAvailable)
             {
-                //Thread.Sleep(10);
+                Thread.Sleep(100);
                 total++;
-                QueryPapers(connection);
-                if (total % 200 == 0)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine(((double) sw.ElapsedMilliseconds/total) + " msec/iteration.");
-                }
+
+
+                Stakan.UpdateStakan("FSMICXM0");
+                Stakan.UpdateStakan("RiM0");
+                //QueryCmdText("select * from Stakan_FSMICXM0 order by Price");
+                //QueryCmdText("select * from stakan_rim0 order by Price");
+                
+                //if (total % 200 == 0)
+                //{
+                //    Console.WriteLine();
+                //    Console.WriteLine(((double)sw.ElapsedMilliseconds / total) + " msec/iteration.");
+                //}
             }
 
             LogManager.Shutdown();
-        }
-
-        class PaperItem : IEquatable<PaperItem>
-        {
-            public string Instrument { private get; set; }
-
-            public double Bid { private get; set; }
-            public double Ask { private get; set; }
-            public double Price { private get; set; }
-
-            public DateTime Time { private get; set; }
-
-            public bool Equals(PaperItem other)
-            {
-                return other != null &&
-                    Instrument == other.Instrument &&
-                    Bid == other.Bid &&
-                    Ask == other.Ask &&
-                    Price == other.Price &&
-                    Time == other.Time;
-            }
-
-            public override string ToString()
-            {
-                return string.Format("{0}: {1}({2},{3}); {4}", Time.ToLongTimeString(), Instrument, Bid, Ask, Price);
-            }
-        }
-
-        static List<PaperItem> _lastItems = new List<PaperItem>();
-        static bool QueryPapers(SQLiteConnection connection)
-        {
-            QueryCmdText(connection, "select * from stakan_eum0 order by price");
-            QueryCmdText(connection, "select * from stakan_edm0 order by price");
-            QueryCmdText(connection, "select * from stakan_sim0 order by price");
-
-            QueryCmdText(connection, "select * from futlimits");
-            QueryCmdText(connection, "select * from micexlimits");
-            QueryCmdText(connection, "select count(*) from orders");
-            QueryCmdText(connection, "select * from portfolio");
-            QueryCmdText(connection, "select count(*) from trades");
-
-            //var command = new SQLiteCommand("select * from papers order by time desc", connection);
-
-            //var reader = command.ExecuteReader();
-
-            //var items = new List<PaperItem>();
-            //while (reader.Read())
-            //{
-            //    var objs = new object[reader.FieldCount];
-            //    reader.GetValues(objs);
-
-            //    items.Add(new PaperItem
-            //    {
-            //      Instrument = (string)objs[0],
-            //      Bid = (double)objs[1],
-            //      Ask = (double)objs[2],
-            //      Price = (double)objs[3],
-            //      Time = (DateTime)objs[4]
-            //    });
-            //}
-
-            //reader.Close();
-
-            //if (!items.SequenceEqual(_lastItems))
-            //{
-            //    _lastItems = items;
-
-            //    Console.Clear();
-            //    foreach (var item in items)
-            //        Console.WriteLine(item);
-
-            //    return true;
-            //}
-            return false;
-        }
-
-        private static void QueryCmdText(SQLiteConnection connection, string cmdText)
-        {
-            var command1 = new SQLiteCommand(cmdText, connection);
-            QueryCommand(command1);
-        }
-
-        static void QueryCommand(SQLiteCommand command)
-        {
-            var reader = command.ExecuteReader();
-
-            while (true)
-            {
-                var objs = new object[reader.FieldCount];
-
-                reader.GetValues(objs);
-
-                if(!reader.Read())
-                    break;
-            }
-
-            reader.Close();
         }
     }
 }
---
+++ branches/slybot2/Service/Stakan.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using log4net;
+
+namespace Service
+{
+    [DebuggerDisplay("Order({Price}, {Quantity}, {MyQuantity})")]
+    public struct StakanOrder
+    {
+        public double Price;
+        public int Quantity;
+        public int MyQuantity;
+        public DateTime Time;
+    }
+
+
+    public class Stakan
+    {
+        static readonly ILog StakansLogger = LogManager.GetLogger("Stakans");
+
+        static Dictionary<string, Stakan> Items = new Dictionary<string, Stakan>();
+
+        public static SQLiteConnection _connection = null;
+
+        private static IEnumerable<object[]> QueryCmdText(string cmdText)
+        {
+            var command = new SQLiteCommand(cmdText, _connection);
+            var reader = command.ExecuteReader();
+
+            while (true)
+            {
+                var objs = new object[reader.FieldCount];
+
+                reader.GetValues(objs);
+
+                yield return objs;
+
+                if (!reader.Read())
+                    break;
+            }
+
+            reader.Close();
+        }
+
+
+        public static void UpdateStakan(string instrument)
+        {
+            var sw = new Stopwatch(); sw.Start();
+
+            Stakan oldStakan = null;
+            Items.TryGetValue(instrument, out oldStakan);
+            
+            var newStakan = new Stakan(instrument);
+            Items[instrument] = newStakan;
+
+            if (oldStakan == null || oldStakan.ToString() != newStakan.ToString())
+                StakansLogger.Debug(newStakan.ToString());
+
+            sw.Stop();
+
+            //if (sw.ElapsedMilliseconds > 300)
+            //    Util.PerformanceLogger.ErrorFormat("Stakan of {0} update took {1} milliseconds.", instrument, sw.ElapsedMilliseconds);
+        }
+
+        //static readonly int readDepth = 10;
+        static readonly int printDepth = 5;
+
+        private Stakan(string instrument)
+        {
+            Instrument = instrument;
+
+            string queryBid = string.Format(
+                @"select Price, Bid, MyBid from stakan_{0} where bid > 0 order by price desc", Instrument);
+
+            string queryAsk = string.Format(
+                @"select Price, Ask, MyAsk from stakan_{0} where ask > 0 order by price asc", Instrument);
+
+            Bids = new List<StakanOrder>();
+            foreach (var item in QueryCmdText(queryBid))
+            {
+                try
+                {
+                    double price = (double)item[0];
+                    int contracts = (int)(long)item[1];
+                    int myContracts = (int)(long)item[2];
+
+                    Bids.Add(new StakanOrder()
+                    {
+                        Price = price,
+                        Quantity = contracts - myContracts,
+                        MyQuantity = myContracts,
+                        Time = DateTime.Now
+                    });
+                }
+                catch
+                {}
+            }
+
+            Asks = new List<StakanOrder>();
+            foreach (var item in QueryCmdText(queryAsk))
+            {
+                try
+                {
+                    double price = (double)item[0];
+                    int contracts = (int)(long)item[1];
+                    int myContracts = (int)(long)item[2];
+
+                    Asks.Add(new StakanOrder()
+                    {
+                        Price = price,
+                        Quantity = contracts - myContracts,
+                        MyQuantity = myContracts,
+                        Time = DateTime.Now
+                    });
+                }
+                catch
+                { }
+            }
+        }
+
+        public string Instrument { get; set; }
+        public List<StakanOrder> Bids { get; set; }
+        public List<StakanOrder> Asks { get; set; }
+        
+        string stringView = null;
+        public override string ToString()
+        {
+            if (stringView == null)
+            {
+                StringBuilder stakanStrBuilder = new StringBuilder();
+
+                stakanStrBuilder.Append(Instrument);
+                stakanStrBuilder.Append(": ");
+                
+                bool first = true;
+                int bidStartIndex = Math.Min(printDepth, Bids.Count) - 1;
+                for (int i = bidStartIndex; i >= 0; --i)
+                {
+                    var item = Bids[i];
+
+                    if (!first)
+                        stakanStrBuilder.Append(" ");
+
+                    stakanStrBuilder.AppendFormat("({0},{1}", item.Price, item.Quantity + item.MyQuantity);
+                    if (item.MyQuantity > 0)
+                        stakanStrBuilder.AppendFormat(",{0})", item.MyQuantity);
+                    else
+                        stakanStrBuilder.AppendFormat(")");
+
+                    first = false;
+                }
+
+                stakanStrBuilder.Append("  # ");
+
+                int askStopIndex = Math.Min(printDepth, Asks.Count);
+                for(int i = 0; i < askStopIndex; ++i)
+                {
+                    var item = Asks[i];
+
+                    stakanStrBuilder.Append(" ");
+
+                    stakanStrBuilder.AppendFormat("({0},{1}", item.Price, item.Quantity + item.MyQuantity);
+                    if (item.MyQuantity > 0)
+                        stakanStrBuilder.AppendFormat(",{0})", item.MyQuantity);
+                    else
+                        stakanStrBuilder.AppendFormat(")");
+                }
+
+                stringView = stakanStrBuilder.ToString();
+            }
+
+            return stringView;
+        }
+    }
+}
